a035788e7bc4607fd28458a6596b74e9d253362a
Cameron Bisca <cjbisca@gmail.com>
2020-11-03 00:22:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  created_array = ["Learning", "is", "cool"]
+  created_array = ["Learning", "is", "really", "cool"]
 end
 
 def add_element_to_end_of_array(array, element)
